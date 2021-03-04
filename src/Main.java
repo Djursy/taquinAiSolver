492d24863b4a0cac3b0214a3d477ad90ee47f574
Nero <nour.ayar@etu.univ-amu.fr>
2021-03-05 00:40:37 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        final InstanceReader instanceReaderTest = new InstanceReader("testTaquin");
-
-        final State initialState = instanceReaderTest.getInitialState();
-        final State finalState = instanceReaderTest.getFinalState();
-
-        final Taquin taquin = new Taquin(initialState, finalState);
-
-        TaquinSolver.bestFirstSearch(taquin);
+        for (int i =1; i<29; i++){
+            final InstanceReader instanceReaderTest = new InstanceReader("test/taquin" + i +".txt");
+            final State initialState = instanceReaderTest.getInitialState();
+            final State finalState = instanceReaderTest.getFinalState();
+            final Taquin taquin = new Taquin(initialState, finalState);
+            TaquinSolver.depthSearch(taquin);
+        }
     }
 }
